074a266cd4a3779cc7544b251d1a4205d645e521
sam <sarmad.zaki@yahoo.com>
2017-02-13 23:46:16 +0500

    project0 and 01

---
+++ public/script.js
@@ -0,0 +1,8 @@
+var heading = React.createClass({
+    render: function(){
+        return(
+            <h1>Sarmad zaki</h1>,
+        );
+    }
+});
+ReactDOM.render(<heading />, document.getElementById('new'));
